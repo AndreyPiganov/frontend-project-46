b91523f9bf092e9d4d3afa49a309a6de5fe57742
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-03 13:06:37 +0300

    update tests

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,14 @@
+const genDiff = require('../src/index.js');
+
+test('gendiff', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
+    `{
+        - follow: false
+          host: hexlet.io
+        - proxy: 123.234.53.22
+        - timeout: 50
+        + timeout: 20
+        + verbose: true
+        }`,
+  );
+});
