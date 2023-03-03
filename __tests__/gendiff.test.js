873ac34a27956d9f7386098ae077a74b7abf51a1
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-03 15:41:25 +0300

    add test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,14 @@
-const genDiff = require('../src/index.js');
+import genDiff from '../src/index.js';
 
 test('gendiff', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
     `{
-        - follow: false
-          host: hexlet.io
-        - proxy: 123.234.53.22
-        - timeout: 50
-        + timeout: 20
-        + verbose: true
-        }`,
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`,
   );
 });
