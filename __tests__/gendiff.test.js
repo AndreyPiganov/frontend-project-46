7f849e935020182ff82d906f3347b0a480ca6297
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-06 13:45:03 +0300

    add new tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -21,4 +21,15 @@ test('gendiff', () => {
 + verbose: true
 }`,
   );
+  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(
+    `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`,
+  );
+  expect(genDiff('__fixtures__/file1.md', '__fixtures__/file2.md')).toThrow();
 });
