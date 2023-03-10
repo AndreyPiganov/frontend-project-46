b1e371d7d466fa2bfabdfccf741237c65a752ed8
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-10 19:19:13 +0300

    refactoring

---
+++ src/formatter/index.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+const diff = (tree) => {
+    const result = tree.flatMap((node) => {
+      const newNode = _.cloneDeep(node);
+      switch (newNode.status) {
+        case 'deleted':
+          newNode[`- ${newNode.key}`] = newNode.value;
+          break;
+        case 'added':
+          newNode[`+ ${newNode.key}`] = newNode.value;
+          break;
+        case 'nested':
+          newNode[newNode.key] = diff(newNode.value);
+          break;
+        case 'unchanged':
+          newNode[newNode.key] = newNode.value;
+          break;
+        default:
+          newNode[`- ${newNode.key}`] = newNode.value1;
+          newNode[`+ ${newNode.key}`] = newNode.value2;
+      }
+      return _.omit(newNode, ['key', 'value', 'status', 'value1', 'value2']);
+    });
+    return result;
+  };
+export default diff;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import parse from './parsers.js';
 import { getFormat, readFile } from './utils.js';
 import buildTree from './buildTree.js';
+import diff from './formatter/index.js';
 
 // eslint-disable-next-line no-unused-vars
 const genDiff = (filePath1, filePath2, format = 'stylish') => {
@@ -12,32 +13,8 @@ const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const obj2 = parse(file2, getFormat(filePath2));
 
   const tree = buildTree(obj1, obj2);
-  const testCase = (tree) => {
-    const result = tree.flatMap((node) => {
-      const newNode = _.cloneDeep(node);
-      switch (newNode.status) {
-        case 'deleted':
-          newNode[`- ${newNode.key}`] = newNode.value;
-          break;
-        case 'added':
-          newNode[`+ ${newNode.key}`] = newNode.value;
-          break;
-        case 'nested':
-          newNode[newNode.key] = testCase(newNode.value);
-          break;
-        case 'unchanged':
-          newNode[newNode.key] = newNode.value;
-          break;
-        default:
-          newNode[`- ${newNode.key}`] = newNode.value1;
-          newNode[`+ ${newNode.key}`] = newNode.value2;
-          break;
-      }
-      return _.omit(newNode, ['key', 'value', 'status', 'value1', 'value2']);
-    });
-    return result;
-  };
-  console.log(JSON.stringify(testCase(tree)));
-  return testCase(tree);
+
+  console.log(JSON.stringify(diff(tree)));
+  return diff(tree);
 };
 export default genDiff;
