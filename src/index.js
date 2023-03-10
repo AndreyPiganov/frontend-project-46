e1354a2326d0588d2ba4afc0a51e9df7ef81cc23
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-10 17:37:04 +0300

    add diffTree

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import parse from './parsers.js';
+import _ from  'lodash';
 import { getFormat, readFile } from './utils.js';
 import buildTree from './buildTree.js';
 
@@ -11,7 +12,30 @@ const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const obj2 = parse(file2, getFormat(filePath2));
 
   const tree = buildTree(obj1, obj2);
-  console.log(JSON.stringify(tree));
-  return tree;
+  const testCase = (tree) => {
+  const result = tree.flatMap((node) => {
+    switch(node.status){
+      case 'deleted':
+        node[`- ${node['key']}`] = node['value'];
+        return _.omit(node, ['key' , 'value', 'status']);
+      case 'added':
+        node[`+ ${node['key']}`] = node['value'];
+        return _.omit(node, ['key' , 'value' , 'status']);
+      case 'nested':
+        node[node['key']] = testCase(node['value']);
+        return _.omit(node, ['key' , 'value' , 'status']);
+      case 'unchanged':
+        node[node['key']] = node['value'];
+        return _.omit(node, ['key' , 'value' , 'status']);
+      default:
+        node[`- ${node['key']}`] = node['value1'];
+        node[`+ ${node['key']}`] = node['value2'];
+        return _.omit(node, ['key' , 'value' , 'status', 'value1', 'value2']);
+    }
+  })
+  console.log(JSON.stringify(result));
+  return result;
+}
+  return testCase(tree);
 };
 export default genDiff;
